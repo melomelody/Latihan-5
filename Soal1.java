131c4013b01554eca6e81c921691d4060ff9fb94
melomelody <melomelody.gm@gmail.com>
2018-01-09 15:44:52 +0700

    Jawaban latihan5 no 1

---
+++ Soal1.java
@@ -0,0 +1,26 @@
+
+import java.util.Map;
+import java.util.HashMap;
+import java.math.BigDecimal;
+
+public class Soal1 {
+
+    public static void main(String[] args){
+        Map<String,String> values = new HashMap<>();
+        values.put("001","Muhammad Yusuf"); 
+        values.put("002","Dimas Maryanto");
+        values.put("006","Hari Sapto Adi");
+        values.put("007","Putri Harahap");
+        values.put("008","Dewa Nyoman Santosa");        
+        values.put("009","Hariaty");
+        values.put("010","Karina Virgi");
+
+        values.forEach((key, value) -> { 
+            System.out.println(
+                String.format(
+                "Nama Saya %s, nipnya %s ",
+                value, key)
+            );
+        });
+    }
+}
